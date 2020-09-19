15f47edc3b78e6adfec1926b86ac8f410bef5d60
Yinghao Zhu <zhuyinghao1999@msn.cn>
2020-09-19 13:24:04 +0800

    ps0 preparation

---
+++ ProblemSet0/cs140-ps0/caltrain.c
@@ -0,0 +1,29 @@
+#include "pintos_thread.h"
+
+struct station {
+	// FILL ME IN
+};
+
+void
+station_init(struct station *station)
+{
+	// FILL ME IN
+}
+
+void
+station_load_train(struct station *station, int count)
+{
+	// FILL ME IN
+}
+
+void
+station_wait_for_train(struct station *station)
+{
+	// FILL ME IN
+}
+
+void
+station_on_board(struct station *station)
+{
+	// FILL ME IN
+}
