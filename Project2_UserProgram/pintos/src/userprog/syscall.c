28c9b42c7599accc1ebfb3b20e925e03b04673c6
Coach257 <45021066+Coach257@users.noreply.github.com>
2020-11-28 00:31:38 +0800

    init syscall

--- Project2_UserProgram/pintos/src/userprog/syscall.c
+++ Project2_UserProgram/pintos/src/userprog/syscall.c
@@ -10,8 +10,13 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "userprog/process.h"
+#include <string.h>
 
 static void syscall_handler (struct intr_frame *);
+void halt(void);
+void exit(int status);
+int exec(const char *cmd_line);
 
 void
 syscall_init (void) 
@@ -22,30 +27,40 @@ syscall_init (void)
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  int *p = f->esp;
+  switch (*p)
+  {
+  case SYS_HALT:
+    halt();
+    break;
+  case SYS_EXIT:
+    exit(*(p+1));
+    break;
+  case SYS_EXEC:
+    exec(*(p+1));
+    break;
+  default:
+    break;
+  }
+  // printf ("system call!\n");
+  // thread_exit ();
 }
 
-// /* Terminates Pintos by calling shutdown_power_off() (declared in devices/shutdown.h). This should be seldom used, because you lose some information about possible deadlock situations, etc. */
+void 
+halt(void)
+{
+  shutdown_power_off();
+}
 
-// void 
-// halt (void)
-// {
-//   shutdown_power_off();  
-// }
+void
+exit(int status)
+{
+  thread_current()->ret = status;
+  thread_exit();
+}
 
-// /* Terminates the current user program, returning status to the kernel. If the process's parent waits for it (see below), this is the status that will be returned. Conventionally, a status of 0 indicates success and nonzero values indicate errors. */
-
-// void 
-// exit (int status)
-// {
-//   // terminates current user program 
-//   // return status to the kernel
-// }
-
-// /* Runs the executable whose name is given in cmd_line, passing any given arguments, and returns the new process's program id (pid). Must return pid -1, which otherwise should not be a valid pid, if the program cannot load or run for any reason. Thus, the parent process cannot return from the exec until it knows whether the child process successfully loaded its executable. You must use appropriate synchronization to ensure this. */
-// pid_t 
-// exec (const char * cmd_line)
-// {
-
-// }
+int
+exec(const char *cmd_line)
+{
+  return process_execute(cmd_line);
+}
--- Project2_UserProgram/pintos/src/userprog/syscall.h
+++ Project2_UserProgram/pintos/src/userprog/syscall.h
@@ -1,10 +1,5 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
-
 void syscall_init (void);
-// void halt(void);
-// void exit(int);
-// pid_t exec(const char*);
-// int wait(pid_t pid);
 
 #endif /* userprog/syscall.h */
