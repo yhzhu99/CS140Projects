0e75835f403b0a391305798dcc10017d085d990d
Coach257 <45021066+Coach257@users.noreply.github.com>
2020-11-30 18:54:28 +0800

    finish exception

--- Project2_UserProgram/pintos/src/userprog/exception.c
+++ Project2_UserProgram/pintos/src/userprog/exception.c
@@ -89,7 +89,8 @@ kill (struct intr_frame *f)
       printf ("%s: dying due to interrupt %#04x (%s).\n",
               thread_name (), f->vec_no, intr_name (f->vec_no));
       intr_dump_frame (f);
-      thread_exit (); 
+      exit(-1);
+      //thread_exit (); 
 
     case SEL_KCSEG:
       /* Kernel's code segment, which indicates a kernel bug.
@@ -104,7 +105,8 @@ kill (struct intr_frame *f)
          kernel. */
       printf ("Interrupt %#04x (%s) in unknown segment %04x\n",
              f->vec_no, intr_name (f->vec_no), f->cs);
-      thread_exit ();
+      exit(-1);
+      //thread_exit ();
     }
 }
 
